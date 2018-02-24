251950716c7abbd77d7c765eef0abb7c2295453c
ChaiYuming <378303946@qq.com>
2018-02-24 10:27:20 +0800

    20180224 给定一个整数数列，找出其中和为特定值的那两个数。 你可以假设每个输入都只会有一种答案，同样的元素不能被重用。
    示例:
    给定 nums = [2, 7, 11, 15], target = 9
    因为 nums[0] + nums[1] = 2 + 7 = 9
    所以返回 [0, 1]

---
+++ src/leetcode/Solution.java
@@ -0,0 +1,38 @@
+package leetcode;
+
+import java.util.Arrays;
+
+public class Solution {  
+    public static int[] twoSum(int[] nums, int target) {
+    	int[] result = new int[2];
+    	int num1 = 0;
+    	int num2 = 0;
+    	String flag = "0";
+    	for(int i=0;i<nums.length;i++) {
+    		num1 = nums[i];
+    		for(int j=0;j<nums.length;j++) {
+    			if(i!=j && nums[i]+nums[j]==target) {
+    				result[1] = j;
+    				flag = "1";
+    				break;
+    			}
+    		}
+    		result[0] = i;
+    		if("1".equals(flag)) {
+    			break;
+    		}
+    	}
+    	return result;
+    }
+    
+    
+    public static void main(String[] args) {
+    	int[] nums = {3,2,4};
+    	int target = 6;
+//    	int[] nums = {2, 7, 11, 15};
+//    	int target = 9;
+        int[] result = new int[2];
+        result = twoSum(nums,target);
+        System.out.println(Arrays.toString(result));
+    }
+}
